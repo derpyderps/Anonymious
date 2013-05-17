79d31905e299816f2e1fce7ab67fe8f8c5e5c211
Vader Anaking <vadertheone@mail.bg>
2013-05-17 23:34:04 +0300

    I AM ANONYMIOUS COMMIT OF log4net project
    I AM ANONYMIOUS

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+using log4net.Config;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+            log.Error("ERROR MESSAGE");
+            log.Info("TESTING LOG4NET");
+            log.Warn("WARNING");
+            log.Info(" LOG4NET");
+            log.Info("Messages are never constantly numered...");
+            log.Warn(log.Logger.Name + " = this logger ");
+        }
+    }
+}
